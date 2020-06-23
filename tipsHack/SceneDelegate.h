7045c7dcbe9808d6180d2ab312ec455246a30f4e
Nicholas Wayoe <nwayoe@fb.com>
2020-06-23 19:44:19 -0400

    Initial Commit

---
+++ tipsHack/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  tipsHack
+//
+//  Created by Nicholas Wayoe on 6/23/20.
+//  Copyright © 2020 Nicholas Wayoe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ tipsHack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipsHack
+//
+//  Created by Nicholas Wayoe on 6/23/20.
+//  Copyright © 2020 Nicholas Wayoe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
